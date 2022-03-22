83b2444670c256eb6c4de3f39fe58f9ed85ede2e
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2022-03-22 10:53:41 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  require 'classes/attack.php';
+  require 'classes/energyType.php';
+  require 'classes/pokemon.php';
+  require 'classes/resistance.php';
+  require 'classes/weakness.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Index</title>
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+</head>
+  <body>
+    <!-- <p>Health Pikachu: ..</p>
+    <p>Health Charmeleon: ..</p>
+    <p>Pikachu valt Charmeleon aan met een Electric Ring attack</p>
+    <p>Charmeleon valt Pikachu aan met een Flare attack</p> -->
+  </body>
+</html>
