29a36ae3806673fe2d97ca8fe9974c1f4f5c967e
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2022-03-22 11:38:01 +0100

    gegevens oproepen via php

--- index.php
+++ index.php
@@ -4,6 +4,11 @@
   require 'classes/pokemon.php';
   require 'classes/resistance.php';
   require 'classes/weakness.php';
+
+  // foreach(glob("classes/*.php") as $filename){
+  //   var_dump($filename);
+  //   include $filename;
+  // }
 ?>
 
 <!DOCTYPE html>
@@ -14,9 +19,9 @@
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 </head>
   <body>
-    <!-- <p>Health Pikachu: ..</p>
-    <p>Health Charmeleon: ..</p>
-    <p>Pikachu valt Charmeleon aan met een Electric Ring attack</p>
-    <p>Charmeleon valt Pikachu aan met een Flare attack</p> -->
+    <p>Health <?php echo $Pikachu->getName();?>: <?php echo $Pikachu->getHealth();?></p>
+    <p>Health <?php echo $Charmeleon->getName();?>: <?php echo $Charmeleon->getHealth();?></p>
+    <p><?php echo $Pikachu->getName();?> valt <?php echo $Charmeleon->getName();?> aan met een <?php echo $Pikachu->getAttack(0);?> attack</p>
+    <p><?php echo $Charmeleon->getName();?> valt <?php echo $Pikachu->getName();?> aan met een <?php echo $Charmeleon->getAttack(1);?> attack</p>
   </body>
 </html>
