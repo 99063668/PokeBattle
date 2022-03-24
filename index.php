a80677b78ad2b884791f10b0af8e93ce19bc33e1
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2022-03-24 12:38:18 +0100

    index bijgwerkt

--- index.php
+++ index.php
@@ -1,14 +1,10 @@
 <?php
-  require 'classes/attack.php';
-  require 'classes/energyType.php';
-  require 'classes/pokemon.php';
-  require 'classes/resistance.php';
-  require 'classes/weakness.php';
+  spl_autoload_register(function ($class) {
+    include 'classes/' . $class . '.php';
+  });
 
-  // foreach(glob("classes/*.php") as $filename){
-  //   var_dump($filename);
-  //   include $filename;
-  // }
+  $Pikachu = new pokemon("Pikachu", new energyType("Lightning", ""), 60, [new attack("Electric Ring", 50), new attack("Pika Punch", 20)], new weakness("Fire", 1.5), new resistance("Fighting", 20));
+  $Charmeleon = new pokemon("Charmeleon", new energyType("Fire", ""), 60, [new attack("Head Butt", 10), new attack("Flare", 30)], new weakness("Water", 2), new resistance("Lightning", 10));
 ?>
 
 <!DOCTYPE html>
@@ -21,7 +17,13 @@
   <body>
     <p>Health <?php echo $Pikachu->getName();?>: <?php echo $Pikachu->getHealth();?></p>
     <p>Health <?php echo $Charmeleon->getName();?>: <?php echo $Charmeleon->getHealth();?></p>
-    <p><?php echo $Pikachu->getName();?> valt <?php echo $Charmeleon->getName();?> aan met een <?php echo $Pikachu->getAttack(0);?> attack</p>
-    <p><?php echo $Charmeleon->getName();?> valt <?php echo $Pikachu->getName();?> aan met een <?php echo $Charmeleon->getAttack(1);?> attack</p>
+
+    <p><?php echo $Pikachu->getName();?> valt <?php echo $Charmeleon->getName();?> aan met een <?php echo $Pikachu->attackDamage($Charmeleon, 0);?> attack</p>
+    <p>Health <?php echo $Charmeleon->getName();?>: <?php echo $Charmeleon->getHealth();?></p>
+    
+    <p><?php echo $Charmeleon->getName();?> valt <?php echo $Pikachu->getName();?> aan met een <?php echo $Charmeleon->attackDamage($Pikachu, 1);?> attack</p>
+    <p>Health <?php echo $Pikachu->getName();?>: <?php echo $Pikachu->getHealth();?></p>
+
+    <p>Aantal levende pokemons: <?php echo $Pikachu->getPopulation();?></p>
   </body>
 </html>
