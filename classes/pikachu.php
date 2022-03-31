053ecfb0e4ef4fb10a3c65a85a0f26a71d5d0c34
Dewi van der Velden <70890464+99063668@users.noreply.github.com>
2022-03-31 13:47:36 +0200

    classes aangepast

--- classes/pikachu.php
+++ classes/pikachu.php
@@ -10,13 +10,24 @@
     }
   }
 
+  namespace extraPokemon1{
+    use pokemon;
+
+    class pikachu extends pokemon{
+      // Constructor
+      public function __construct(){
+        parent::__construct("Pikachu",  new \energyType("Lightning", ""), 80, [new \attack("Electric Ring", 50), new \attack("Pika Punch", 20)], new \weakness("Fire", 1.5), new \resistance("Fighting", 20));
+      }
+    }
+  }
+
   namespace pokemon2{
     use pokemon;
 
     class charmeleon extends pokemon{
       // Constructor
       public function __construct(){
-        parent::__construct("Charmeleon",  new \energyType("Fire", ""), 60, [new \attack("Head Butt", 45), new \attack("Flare", 15)], new \weakness("Water", 1), new \resistance("Lightning", 35));
+        parent::__construct("Charmeleon",  new \energyType("Fire", ""), 60, [new \attack("Head Butt", 45), new \attack("Flare", 15)], new \weakness("Water", 1), new \resistance("Lightning", 15));
       }
     }
   }
@@ -27,7 +38,7 @@
     class gogoat extends pokemon{
       // Constructor
       public function __construct(){
-        parent::__construct("GoGoat",  new \energyType("Grass", ""), 60, [new \attack("Earthquake", 30), new \attack("Hyper Beam", 15)], new \weakness("Fire", 1.5), new \resistance("Water", 20));
+        parent::__construct("GoGoat",  new \energyType("Grass", ""), 60, [new \attack("Earthquake", 30), new \attack("Hyper Beam", 15)], new \weakness("Fire", 1.5), new \resistance("Water", 10));
       }
     }
   }
@@ -38,7 +49,7 @@
     class blastoise extends pokemon{
       // Constructor
       public function __construct(){
-        parent::__construct("Blastoise",  new \energyType("Water", ""), 60, [new \attack("Water Gun", 50), new \attack("Blizzard", 25)], new \weakness("Grass", 1), new \resistance("Steel", 35));
+        parent::__construct("Blastoise",  new \energyType("Water", ""), 60, [new \attack("Water Gun", 50), new \attack("Blizzard", 25)], new \weakness("Grass", 1), new \resistance("Steel", 20));
       }
     }
   }
